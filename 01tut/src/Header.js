82478ce78efee3f506f78a4a303e098a9f40168f
Sriteja Somepalli <sritejamahadevan@gmail.com>
2024-04-19 13:53:26 +0200

    clickevents

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,10 +1,28 @@
  const Header = () => {
-    const nameHandler = () => {
+    const handleNameChange = () => {
     const names = ['shravya', 'Teja', 'Aarya'] 
     return names[Math.floor(Math.random() * 3)]
   }
+
+  const handleClick1 = () => {
+    console.log('button click');
+  }
+
+  const handleClick2 = (e) => {
+    console.log('button click', e);
+  }  
+  const handleClick3 = (e) => {
+    console.log(e);
+    console.log(e.target.innerText);
+  }
+  
   return (
-    <header>Hi {nameHandler()}</header>
+    <main>
+    <header onDoubleClick={handleClick1}>Hi {handleNameChange()}</header>
+    <button onClick={handleClick1}>click1</button>
+    <button onClick={() => handleClick2('Teja')}>click2</button>
+    <button onClick={(e) => handleClick3(e)}>click3</button>
+    </main>
   )
 }
 
